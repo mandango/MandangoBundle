5987ddb41c81d73f26180ff84fc22cc5cc9deccc
pablodip <pablodip@gmail.com>
2012-05-02 13:34:59 +0200

    added command to fix the missing references

---
+++ Command/FixMissingReferencesCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of Mandango.
+ *
+ * (c) Pablo Díez <pablodip@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Mandango\MandangoBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * FixMissingReferencesCommand.
+ *
+ * @author Pablo Díez <pablodip@gmail.com>
+ */
+class FixMissingReferencesCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('mandango:fix-missing-references')
+            ->setDescription('Fix missing references.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('ensuring the indexes');
+
+        $this->getContainer()->get('mandango')->fixAllMissingReferences();
+    }
+}
